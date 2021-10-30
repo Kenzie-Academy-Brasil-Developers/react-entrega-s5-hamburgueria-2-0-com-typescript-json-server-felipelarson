1f5e15e8b97558b686529df02b45765264755f6d
Felipe Silveira <felipelarson@msn.com>
2021-10-30 15:08:54 +0100

    style: style login, position side to side

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -22,19 +22,26 @@ export default function Login() {
         <CssBaseline />
         <Grid
           item
-          xs={false}
+          xs={12}
           sm={4}
           md={6}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            // backgroundImage: 'url(https://source.unsplash.com/random)',
+            // backgroundRepeat: 'no-repeat',
+            // backgroundColor: (t) =>
+            //   t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column'
           }}
-        />
+        >
+          <img src={brand} alt="Brand"/>
+        </Grid>
         <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+          
           <Box
             sx={{
               display: 'flex',
@@ -44,11 +51,10 @@ export default function Login() {
               height: '100%'
             }}
           >
-            <img src={brand} alt="Brand"/>
-            <Typography component="h1" variant="h5" sx={{mt: 1}}>
-                Login
-            </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <Typography component="h1" variant="h5" sx={{mt: 1}}>
+                Login
+              </Typography>
               <TextField
                 margin="normal"
                 required
