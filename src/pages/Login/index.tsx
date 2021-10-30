52e906050ccde487cc1b7af7df6bbfbfe358c728
Felipe Silveira <felipelarson@msn.com>
2021-10-30 09:55:52 +0100

    refactor: change position login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import brand from './../../assets/BurguerKenzie.png'
 
 export default function Login() {
+  
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -23,7 +24,7 @@ export default function Login() {
           item
           xs={false}
           sm={4}
-          md={7}
+          md={6}
           sx={{
             backgroundImage: 'url(https://source.unsplash.com/random)',
             backgroundRepeat: 'no-repeat',
@@ -33,18 +34,18 @@ export default function Login() {
             backgroundPosition: 'center',
           }}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
-              mx: 4,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%'
             }}
           >
             <img src={brand} alt="Brand"/>
-            <Typography component="h1" variant="h5" sx={{marginTop: "1rem"}}>
+            <Typography component="h1" variant="h5" sx={{mt: 1}}>
                 Login
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
@@ -78,8 +79,8 @@ export default function Login() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Crie sua conta para saborear muitas delíciase matar sua fome
+                  <Link href="#" variant="subtitle1">
+                    Crie sua conta para saborear muitas delícias e matar sua fome
                   </Link>
                 </Grid>
               </Grid>
