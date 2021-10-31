f7adf88efebdf45be304f9c436428b218f4eb67a
Felipe Silveira <felipelarson@msn.com>
2021-10-31 14:20:19 +0000

    refactor: writed text

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { useModalCart } from '../../Provider/ModalCart';
+import { AppBar } from '@mui/material';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -10,7 +11,6 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
@@ -21,21 +21,24 @@ export default function Cart() {
 
   return (
     <div>
-      <Modal
+        <Modal
         open={openModalCart}
         onClose={handleOpenModalCart}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+        >
+            <Box sx={style}>
+                <AppBar>
+                    <Typography>Carrinho de compras</Typography> 
+                </AppBar>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Sua sacola está vazia
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Adicione itens
+                </Typography>
+            </Box>
+        </Modal>
     </div>
   );
 }
