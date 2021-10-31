091259dea9affe7c09d5a619f21f885315a1ff0e
Felipe Silveira <felipelarson@msn.com>
2021-10-31 21:57:50 +0000

    feat: added button to add and sub

--- src/components/ProductsFromCart/index.tsx
+++ src/components/ProductsFromCart/index.tsx
@@ -1,17 +1,17 @@
 import Box from '@mui/material/Box';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { IconButton } from '@mui/material';
+import { Button, ButtonGroup, IconButton } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function ProductsFromCart() {
   return (
     <Box
       sx={{
         display: 'flex',
-        flexDirection: { xs: 'column', md: 'row' },
         alignItems: 'center',
         bgcolor: 'background.paper',
         overflow: 'hidden',
-        borderRadius: '12px',
         fontWeight: 'bold',
       }}
     >
@@ -20,8 +20,6 @@ export default function ProductsFromCart() {
         sx={{
           height: 80,
           width: 80,
-          maxHeight: { xs: 80, md: 80 },
-          maxWidth: { xs: 80, md: 80 },
         }}
         alt="The house from the offer."
         src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
@@ -30,14 +28,19 @@ export default function ProductsFromCart() {
         sx={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          alignItems: { xs: 'center', md: 'flex-start' },
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
           m: 3,
-          minWidth: { md: 350 },
+          minWidth: 'calc(100% - 30%)',
         }}
       >
-        <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
+        <Box component="span" sx={{ fontSize: 16, mt: 1, display: 'flex', flexDirection: 'column', }}>
           Título
+        <ButtonGroup variant="contained" size="small" aria-label="outlined primary button group" sx={{mt:2,boxShadow: 0}}>
+            <Button><RemoveIcon/></Button>
+            <Button variant="outlined">1</Button>
+            <Button><AddIcon /></Button>
+        </ButtonGroup>
         </Box>
         <IconButton>
             <DeleteIcon />
